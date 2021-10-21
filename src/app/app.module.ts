1703d8cb22e929e5bb4e60b8b89c0899b3c9c266
Matoufk <mathis.leclerc@etudiant.univ-rennes1.fr>
2021-10-21 10:41:17 +0200

    Fix import probleme home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomePage } from './home/home.page';
 import { EdtPage } from './edt/edt.page';
 import { ContactPage } from './contact/contact.page';
 import { MapPage } from './map/map.page';
 
 @NgModule({
-  declarations: [AppComponent, HomePage, EdtPage, ContactPage, MapPage],
+  declarations: [AppComponent, EdtPage, ContactPage, MapPage],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
