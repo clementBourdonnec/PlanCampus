2a1c6bcdf29b2337fd787fa2bd25974a7110d16e
MateoFontanel <fontanelmateo@gmail.com>
2021-12-16 09:42:08 +0100

    Ajout requêtes serveur + infinite scrollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { EdtPage } from './edt/edt.page';
 import { ContactPage } from './contact/contact.page';
 import { MapPage } from './map/map.page';
 
+import { HttpClientModule } from '@angular/common/http'
+
+
 @NgModule({
   declarations: [AppComponent, EdtPage, ContactPage, MapPage],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpConfigService } from '../services/http-config.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  
+  url: string;
+  itemListData = [];
+  page_number = 1;
+  page_limit = 8;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private httpConfigService: HttpConfigService
+    ) { }
+
+  ngOnInit() {  
+    this.getEmployees(false, "");
   }
 
+  getEmployees(isFirstLoad, event) {
+
+    this.url = '?_page=' + this.page_number + '&_limit=' + this.page_limit;
+
+    this.httpConfigService.getListItems(this.url)
+      .subscribe((data: any) => {
+
+        for (let i = 0; i < data.length; i++) {
+          this.itemListData.push(data[i]);
+        }
+
+        if (isFirstLoad)
+          event.target.complete();
+
+        this.page_number++;
+      }, error => {
+        console.log(error);
+      })
+  }
+
+  doInfinite(event) {
+    this.getEmployees(true, event);
+  }
 }
