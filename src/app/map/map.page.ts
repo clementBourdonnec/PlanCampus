066799223167029d615f2f86d57027430a406256
Matoufk <mathis.leclerc@etudiant.univ-rennes1.fr>
2022-01-13 10:28:52 +0100

    Centrage de la carte sur Beaulieu

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -16,17 +16,17 @@ export class MapPage implements OnInit,OnDestroy {
   ionViewDidEnter() { this.leafletMap(); }
 
   leafletMap() {
-    this.map = Leaflet.map('mapId').setView([28.644800, 77.216721], 5);
+    this.map = Leaflet.map('mapId').setView([	48.117266, -1.64], 16); //Latitude/longitude/zoomlevel
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'edupala.com © Angular LeafLet',
     }).addTo(this.map);
 
-    Leaflet.marker([28.6, 77]).addTo(this.map).bindPopup('Delhi').openPopup();
-    Leaflet.marker([34, 77]).addTo(this.map).bindPopup('Leh').openPopup();
+    // Leaflet.marker([28.6, 77]).addTo(this.map).bindPopup('Delhi').openPopup();
+    // Leaflet.marker([34, 77]).addTo(this.map).bindPopup('Leh').openPopup();
 
-    antPath([[28.644800, 77.216721], [34.1526, 77.5771]],
-      { color: '#FF0000', weight: 5, opacity: 0.6 })
-      .addTo(this.map);
+    // antPath([[28.644800, 77.216721], [34.1526, 77.5771]],
+      // { color: '#FF0000', weight: 5, opacity: 0.6 })
+      // .addTo(this.map);
   }
 
   /** Remove map when we have multiple map object */
