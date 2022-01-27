c35c4e6d7714be20a0794bd5ff4b1f31a948a771
Matéo Fontanel <fontanelmateo@gmail.com>
2022-01-27 12:48:38 +0100

    Enregistrement de nouveaux contacts fonctionnel

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(bodyParser.urlencoded({
  app.use(bodyParser.json());
 
 
-app.post('*', function(req, res) {
+ // Quand l'utilisateur enregistre un nouveau contact
+app.post('/contact', function(req, res) {
     console.log("post reçu sur "+req.url)
     let body = req.body
     var new_contact = {"name":body.name,
@@ -25,7 +26,7 @@ app.post('*', function(req, res) {
                        "email":body.email,
                        "phone":body.phone,
                        "infos":body.infos}
-
+    console.log(new_contact)
     var contacts = JSON.parse(fs.readFileSync("serverdata/contacts.json", 'utf8'))
     contacts.push(new_contact)
     fs.writeFileSync("serverdata/contacts.json", JSON.stringify(contacts), 'utf-8')
@@ -35,7 +36,34 @@ app.post('*', function(req, res) {
 
 app.get('*', function(req, res){
     console.log("get reçu sur "+req.url)
-    res.sendFile(path.join(__dirname, baseDir, 'index.html'));
+
+    var route = req.url.split('/')
+    var routeRoot = route[1]
+
+    switch(routeRoot){
+
+        case "contact":
+            var requestedId = route[2] // /contact/2 -> ['','contact','2']
+            var contacts = JSON.parse(fs.readFileSync("serverdata/contacts.json", 'utf8'))
+            if(Number(requestedId) >= contacts.length) requestedId = contacts.length - 1
+
+            var requestedContact = contacts[requestedId]
+            console.log(requestedContact)
+            var htmlFile = fs.readFileSync(path.join(__dirname, baseDir, 'index.html'), 'utf-8')
+            htmlFile.replace(`id="name"`,`id="name" value="`+requestedContact.name+`"`)
+            htmlFile.replace(`id="address"`,`id="address" value="`+requestedContact.address+`"`)
+            htmlFile.replace(`id="email"`,`id="email" value="`+requestedContact.email+`"`)
+            htmlFile.replace(`id="phone"`,`id="phone" value="`+requestedContact.phone+`"`)
+            htmlFile.replace(`id="infos"`,`id="infos" value="`+requestedContact.infos+`"`)
+            // TODO : trouver un outil de templating qui permette d'envoyer la page modifiée
+            res.send("ici la reponse")
+            break;
+
+        default:
+            res.sendFile(path.join(__dirname, baseDir, 'index.html'));
+            break;
+    }
+
 })
 
 app.listen(8000, function() {
